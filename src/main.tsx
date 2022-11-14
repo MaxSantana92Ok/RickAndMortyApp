27812f9f60f2697c5be2704cf74fb3c772ba907c
Max Santana <maxsantana92ok@gmail.com>
2022-11-14 18:13:44 -0300

    Added AppProvider and SWRConfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import {AppProvider} from './store/AppProvider';
 import CustomTheme from './styles/CustomTheme';
+import {SWRConfig} from 'swr';
+import API from './api/config';
+import {Backdrop, CircularProgress} from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <CustomTheme>
-      <App />
-    </CustomTheme>
+    <AppProvider>
+      <CustomTheme>
+        <SWRConfig
+          value={{fetcher: (url: string) => API(url).then(res => res.data), suspense: true}}
+        >
+          <App />
+        </SWRConfig>
+      </CustomTheme>
+    </AppProvider>
   </React.StrictMode>
 );
