b4f886a1bebcca7ed7b919dc9b69f5ec4435b2c4
Max Santana <maxsantana92ok@gmail.com>
2022-11-14 18:13:59 -0300

    Added home to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,13 @@
-import {useLayoutEffect} from 'react';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import {getCharacter_Service} from './services/services';
+import {Suspense} from 'react';
+import Home from './components/pages/Home';
+import Public from './components/pages/Public';
 
 function App() {
-  useLayoutEffect(() => {
-    getCharacter_Service();
-    return () => {};
-  }, []);
-
   return (
     <>
-      <Typography variant="h3">Hellow World</Typography>
-      <Typography variant="h6">Hellow World</Typography>
-      <Typography variant="subtitle1">Hellow World</Typography>
-      <Typography variant="body1">Hellow World</Typography>
-      <Stack spacing={2} direction="row">
-        <Button variant="contained" color="primary">
-          Contained
-        </Button>
-        <Button variant="outlined" color="secondary">
-          Contained
-        </Button>
-        <Button variant="text" color="primary">
-          Text
-        </Button>
-        <Button variant="text" color="neutral">
-          Text
-        </Button>
-        <Button variant="contained" color="success">
-          Outlined
-        </Button>
-        <Button variant="contained" color="error">
-          Outlined
-        </Button>
-      </Stack>
+      <Suspense fallback={<>Loading</>}>
+        <Home />
+      </Suspense>
     </>
   );
 }
