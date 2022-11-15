71c02a4418d254ef6e6104621b39b0fef2a44732
Max Santana <maxsantana92ok@gmail.com>
2022-11-14 22:06:15 -0300

    deleted code unsed

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
@@ -6,14 +6,16 @@ import {AppProvider} from './store/AppProvider';
 import CustomTheme from './styles/CustomTheme';
 import {SWRConfig} from 'swr';
 import API from './api/config';
-import {Backdrop, CircularProgress} from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AppProvider>
       <CustomTheme>
         <SWRConfig
-          value={{fetcher: (url: string) => API(url).then(res => res.data), suspense: true}}
+          value={{
+            fetcher: (url: string) => API(url).then(res => res.data),
+            suspense: true,
+          }}
         >
           <App />
         </SWRConfig>
