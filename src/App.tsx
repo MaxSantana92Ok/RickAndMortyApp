9e30af407ee179643ac198e071e5769cb0ce6636
Max Santana <maxsantana92ok@gmail.com>
2022-11-14 22:05:53 -0300

    Added loading to config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import {Backdrop, CircularProgress} from '@mui/material';
 import {Suspense} from 'react';
 import Home from './components/pages/Home';
-import Public from './components/pages/Public';
 
 function App() {
   return (
     <>
-      <Suspense fallback={<>Loading</>}>
+      <Suspense
+        fallback={
+          <div>
+            <Backdrop sx={{color: '#fff', zIndex: theme => theme.zIndex.drawer + 1}} open={true}>
+              <CircularProgress color="inherit" />
+            </Backdrop>
+          </div>
+        }
+      >
         <Home />
       </Suspense>
     </>
