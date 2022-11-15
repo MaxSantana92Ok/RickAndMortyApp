fa21f9941a7353e7f5745a3617da86ed7074b4f1
Max Santana <maxsantana92ok@gmail.com>
2022-11-14 22:08:09 -0300

    Fix Pagination, Added Episode Item

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,4 @@
-import {useContext, useLayoutEffect, useState} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import {AppContext} from '../../store/AppContext';
 import useSWR from 'swr';
 import {Box} from '@mui/system';
@@ -6,6 +6,8 @@ import CardItem from '../organism/CardItem';
 import {Result} from '../../interface/interface';
 import './Home.css';
 import Paging from '../organism/Paging';
+import {Typography} from '@mui/material';
+import EpisodeItem from '../organism/EpisodeItem';
 
 interface Characters {
   first: Result[];
@@ -14,61 +16,95 @@ interface Characters {
 
 const Home = () => {
   const {appStore, setCharacterList_Store, setPageList_Store} = useContext(AppContext);
-  const {list, page} = appStore;
+  const {list, page, character1, character2} = appStore;
   const [characters, setCharacters] = useState<Characters>({
     first: [],
     second: [],
   });
   const [goPage, setGoPage] = useState(1);
-  let {data, isValidating} = useSWR(`/character/?page=${goPage}`);
 
-  const handleGetPage = (page: number) => {
-    console.log('Handle PAge', page);
-    setGoPage(page);
+  let {data, isValidating, error} = useSWR(
+    `https://rickandmortyapi.com/api/character?page=${goPage}`
+  );
+
+  const handleGetPage = (n: number) => {
+    setGoPage(n);
   };
 
-  useLayoutEffect(() => {
-    if (data) {
-      setCharacterList_Store(data.results);
-      setPageList_Store(data.info);
+  useEffect(() => {
+    if (!error) {
+      if (data) {
+        setCharacterList_Store(data.results);
+        setPageList_Store(data.info);
+      }
+      if (data) {
+        const half = Math.floor(data.results.length / 2);
+        setCharacters({
+          first: data.results.slice(0, half),
+          second: data.results.slice(half),
+        });
+      }
     }
-    if (list.length > 0) {
-      const half = Math.floor(list.length / 2);
-      setCharacters({
-        first: list.slice(0, half),
-        second: list.slice(half),
-      });
-    }
-    return () => {};
-  }, [list.length]);
 
-  return (
-    <Box>
-      <Box className="Box-Character-List">
-        <Box display="flex" flexDirection="column" width="100%" p={1}>
-          {characters.first?.map((item, index) => {
-            return (
-              <Box margin={1} key={index}>
-                <CardItem item={item} type="one" />
-              </Box>
-            );
-          })}
+    return () => {};
+  }, [data, error]);
+
+  if (error) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center" width="100%" height="100vh">
+        <Typography variant="h3"> Sorry, no fun at the moment!</Typography>
+      </Box>
+    );
+  }
+
+  if (!error) {
+    return (
+      <Box>
+        <Box className="Box-Character-List">
+          <Box display="flex" flexDirection="column" width="100%" p={1}>
+            {characters.first?.map((item, index) => {
+              return (
+                <Box margin={1} key={index}>
+                  <CardItem item={item} type="one" />
+                </Box>
+              );
+            })}
+          </Box>
+          <Box display="flex" flexDirection="column" width="100%" p={1}>
+            {characters.second?.map((item, index) => {
+              return (
+                <Box margin={1} key={index}>
+                  <CardItem item={item} type="two" />
+                </Box>
+              );
+            })}
+          </Box>
         </Box>
-        <Box display="flex" flexDirection="column" width="100%" p={1}>
-          {characters.second?.map((item, index) => {
-            return (
-              <Box margin={1} key={index}>
-                <CardItem item={item} type="two" />
-              </Box>
-            );
-          })}
+        <Box className="Box-Pagination">
+          <Paging getPage={handleGetPage} cant={page.pages} isDisabled={isValidating} />
+        </Box>
+        <Box className="Box-Episodes">
+          {character1 && (
+            <Box display="flex" flexDirection="column">
+              <Typography variant="h6">{character1.name}</Typography>
+              {character1.episode.map((epi, index) => {
+                return <EpisodeItem key={index} episode={epi} />;
+              })}
+            </Box>
+          )}
+          {character2 && (
+            <Box display="flex" flexDirection="column">
+              <Typography variant="h6">{character2.name}</Typography>
+              {character2.episode.map((epi, index) => {
+                return <EpisodeItem key={index} episode={epi} />;
+              })}
+            </Box>
+          )}
         </Box>
       </Box>
-      <Box className="Box-Pagination">
-        <Paging getPage={handleGetPage} cant={page.pages} isDisabled={isValidating} />
-      </Box>
-    </Box>
-  );
+    );
+  }
+  return <></>;
 };
 
 export default Home;
